5113c025991842eeff2aabed5526e654b76fe7b9
Lucas Barque <lucasbarquedasilva@hotmail.com>
2019-04-11 22:44:25 -0400

    Scroll Horizontal

---
+++ Assets/Scripts/scrollhorizontal.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scrollhorizontal : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float velocity = 0.1f;
+    public Renderer quad;
+    void Start() {
+        
+    }
+    
+    void Update() {
+        Vector2 offset = new Vector2(velocity * Time.deltaTime, 0);
+
+        quad.material.mainTextureOffset += offset;
+    }
+}
